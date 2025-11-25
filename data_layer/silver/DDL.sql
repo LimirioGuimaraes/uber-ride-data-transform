6f22d0377baba5cd76f9794a4e76988df0ddbada
Breno <brenob6@gmail.com>
2025-11-25 19:14:11 -0300

    chore: rename columns

--- data_layer/silver/DDL.sql
+++ data_layer/silver/DDL.sql
@@ -1,38 +1,38 @@
-CREATE TYPE bkg_stt_enum AS ENUM (
+CREATE TYPE booking_status_enum AS ENUM (
     'No Driver Found', 'Incomplete', 'Completed', 
     'Cancelled by Driver', 'Cancelled by Customer'
 );
 
-CREATE TYPE ccd_by_enum AS ENUM (
+CREATE TYPE cancelled_by_enum AS ENUM (
     'customer', 'driver', 'none'
 );
 
-CREATE TYPE irr_enum AS ENUM (
+CREATE TYPE incomplete_ride_reason_enum AS ENUM (
     'Vehicle Breakdown', 'Other Issue', 'Customer Demand'
 );
 
-CREATE TYPE rfc_enum as ENUM (
+CREATE TYPE reason_for_cancelling_enum as ENUM (
 		'Driver is not moving towards pickup location', 'Driver asked to cancel', 'AC is not working', 
     'Change of plans', 'Wrong Address', 'Personal & Car related issues', 'Customer related issue', 
     'More than permitted people in there', 'The customer was coughing/sick'
 );
 
 CREATE TABLE uber_silver (
-	bkg_id CHAR(10) PRIMARY KEY,
-	cus_id CHAR(10),
-	bkg_vle INTEGER,
-	drv_rtg FLOAT,
-	cus_rtg FLOAT,
-	rid_dis FLOAT,
-	pay_mtd VARCHAR(20),
-	dtt TIMESTAMP,
-	veh_typ VARCHAR(20),
-	pic_loc VARCHAR(255),
-	drp_loc VARCHAR(255),
-	avg_vtt FLOAT,
-	avg_ctt FLOAT,
-	ccd_by ccd_by_enum,
-	rfc rfc_enum,
-	bkg_stt bkg_stt_enum,
-	irr irr_enum
+    booking_id CHAR(10) PRIMARY KEY,              
+    customer_id CHAR(10),                         
+    booking_value INTEGER,                        
+    driver_rating FLOAT,                          
+    customer_rating FLOAT,                        
+    ride_distance FLOAT,                          
+    payment_method VARCHAR(20),                   
+    date_time TIMESTAMP,                          
+    vehicle_type VARCHAR(20),                     
+    pickup_location VARCHAR(255),                 
+    drop_location VARCHAR(255),                   
+    avg_vtat FLOAT,                               
+    avg_ctat FLOAT,                               
+    cancelled_by cancelled_by_enum,                     
+    reason_for_cancelling reason_for_cancelling_enum,               
+    booking_status booking_status_enum, 
+    incomplete_ride_reason incomplete_ride_reason_enum 
 );
